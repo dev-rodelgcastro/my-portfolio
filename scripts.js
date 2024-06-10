766a2a363414fa0ca57b7d532ac5dfc34ca90395
dev-rodelgcastro <dev.rodelgcastro@gmail.com>
2024-06-10 23:56:24 +0800

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message!');
+    // Here, you could add code to actually send the form data to your email or server.
+});
